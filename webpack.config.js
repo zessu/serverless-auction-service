1bed38494385e72ed59d8b9156966850c503cc86
Wambua Makenzi <amackzie@gmail.com>
2020-08-08 18:05:54 +0300

    (init): init project

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var nodeExternals = require('webpack-node-externals')
+const path = require('path');
+
+module.exports = {
+   mode: "production",
+   entry: './handler.js',
+   target: 'node',
+   externals: [nodeExternals()],
+   output: {
+      libraryTarget: 'commonjs',
+      path: path.resolve(__dirname, '.webpack'),
+      filename: 'handler.js', // this should match the first part of function handler in serverless.yml
+   },
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude:  path.resolve(__dirname, "node_modules"),
+            loader: ["babel-loader"]
+         }
+      ]
+   }
+};
